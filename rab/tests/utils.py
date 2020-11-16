2aaf79132f726194e94b8c5ff900ff2af013a758
Cooper Lees <me@cooperlees.com>
2020-11-15 17:04:50 -0800

    Support macosx + linux echo path for unittests

--- rab/tests/utils.py
+++ rab/tests/utils.py
@@ -2,8 +2,12 @@
 
 import asyncio
 import unittest
+from platform import system
 
 from rab.utils import gen_check_output
+
+
+MACOSX = system() == "Darwin"
 
 
 class UtilsTests(unittest.TestCase):
@@ -11,10 +15,11 @@ class UtilsTests(unittest.TestCase):
         self.loop = asyncio.get_event_loop()
 
     def test_gen_check_output(self) -> None:
+        echo_path = "/usr/bin/echo" if MACOSX else "/bin/echo"
         expected = (b"unittest\n", None)
         self.assertEqual(
             expected,
             self.loop.run_until_complete(
-                gen_check_output(("/usr/bin/echo", "unittest"), 1)
+                gen_check_output((echo_path, "unittest"), 1)
             ),
         )
